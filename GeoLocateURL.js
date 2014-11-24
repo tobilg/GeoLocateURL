bc59f46a6a698d7077e6055e44e21c88e91e662b
FB Tools <fb.tools.github@gmail.com>
2014-11-24 11:13:49 +0100

    Bugfixes and additional city information

--- GeoLocateURL.js
+++ GeoLocateURL.js
@@ -3,46 +3,63 @@ var dns = require("dns");
 var urlModule = require('url');
 
 var mmdbreader = require('maxmind-db-reader');
-var countries = mmdbreader.openSync('./db/GeoLite2-Country.mmdb');
 
+//URL object
 var urls = { length: 0 }
+
+//MaxMind GeoIP database supported languages
+var allowedLanguages = ["de", "en", "es", "fr", "ja", "pt-br", "ru", "zh-cn"];
+
+//Determine result type from package.json or use city as default
+var geoIpType = process.env.npm_package_config_type || "city";
+var geoIp = (geoIpType === "city" ? mmdbreader.openSync('./db/GeoLite2-City.mmdb') : mmdbreader.openSync('./db/GeoLite2-Country.mmdb'));
 
 http.createServer(function(req,res) {
 
-    var url_parts = urlModule.parse(req.url, true);
-    var queryString = url_parts.query;
-    var protomatch = /^(https?|ftp):\/\//;
-
-    var url = queryString["url"].replace(protomatch,"");
-    url = url.split("/")[0];
-
-    var finished = function() {
+    var getGeoInfo = function() {
 
         var responseObj = {};
         var responseStatus = 200;
 
-        if(urls[url] === "undefined") {
-            responseStatus = 404;
-            responseObj.error = "The URL " + url + " could not be found!";
-        } else {
-
-            var geodata = countries.getGeoDataSync(urls[url]);
+        geoIp.getGeoData(urls[url], function(err, geodata){
 
             responseObj.url = url;
             responseObj.ip = urls[url];
 
             if (geodata !== null && typeof geodata !== "undefined") {
-                responseObj.isoCode = geodata.country.iso_code;
-                responseObj.country = geodata.country.names.en;
+                responseObj.country = geodata.country.names[language];
+                responseObj.countryIsoCode = geodata.country.iso_code;
+
+                if(geoIpType === "city") {
+                    if (geodata.city && geodata.city.names[language]) {
+                        responseObj.city = geodata.city.names[language];
+                    }
+                    if (geodata.subdivisions && geodata.subdivisions.length >= 1 && geodata.subdivisions[0].names[language]) {
+                        responseObj.subdivision = geodata.subdivisions[0].names[language];
+                    }
+                    if (geodata.location && geodata.location.latitude && geodata.location.longitude) {
+                        responseObj.location = {lat: geodata.location.latitude, lng: geodata.location.longitude};
+                    }
+                }
+
             }
 
-        }
+            if (err) {
+                responseObj.error = err;
+                responseStatus = 500;
+            }
 
-        res.writeHead(responseStatus, {'Content-Type': 'application/json'});
-        res.write(JSON.stringify(responseObj));
-        res.end();
+            respond(responseObj, responseStatus);
+
+        });
 
     };
+
+    var respond = function(responseObj, responseStatus) {
+        res.writeHead(responseStatus, {'Content-Type': 'application/json; charset=utf-8'});
+        res.write(JSON.stringify(responseObj));
+        res.end();
+    }
 
     var cb = function(err, adress, family) {
         if(url != "favicon.ico") {
@@ -51,9 +68,28 @@ http.createServer(function(req,res) {
             };
             urls[url] = String(adress);
         };
-        finished();
+
+        //Check whether the IP address could be determined from the URL provided. If not, display error message
+        if (typeof adress === "undefined" || err) {
+            respond({error: "Could not identify IP address from URL " + url + " Please check whether this is a correct URL."}, 404)
+        } else {
+            getGeoInfo();
+        }
     };
 
-    dns.lookup(url, cb);
+    var url_parts = urlModule.parse(req.url, true);
+    var queryString = url_parts.query;
+
+    var url = queryString.url;
+    var language = (queryString.lang ? queryString.lang.toLowerCase() : "en");
+
+    if (url) {
+
+        url = url.replace(/^(https?|ftp):\/\//,"").split("/")[0];
+        language = (allowedLanguages.indexOf(language) > -1 ? language.toLowerCase() : "en");
+
+        dns.lookup(url, cb)
+
+    }
 
 }).listen(process.env.npm_package_config_port || 3000);
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GeoLocateURL
 
-GeoLocateURL is a WebService implemented with NodeJS which takes an URL and returns the URL's IP address, ISO country code and country name if found. It utilizes the MaxMind GeoLite2 Free database available at [http://dev.maxmind.com/geoip/geoip2/geolite2/](http://dev.maxmind.com/geoip/geoip2/geolite2/) for its IP lookups.
+GeoLocateURL is a WebService implemented with NodeJS which takes an URL and returns the URL's IP address, ISO country code and country name and other information if found. It utilizes the MaxMind GeoLite2 Free database available at [http://dev.maxmind.com/geoip/geoip2/geolite2/](http://dev.maxmind.com/geoip/geoip2/geolite2/) for its IP lookups.
 
 ## Installation
 
@@ -29,8 +29,33 @@ To access the WebService via a browser, use `http://localhost:8080/?url=http://w
 ```javascript
 {
     "url": "www.google.com",
-    "ip": "74.125.230.83",
-    "isoCode": "US",
-    "country": "United States"
+    "ip": "173.194.39.19",
+    "country": "United States",
+    "countryIsoCode": "US",
+    "city": "Mountain View",
+    "subdivision": "California",
+    "location": {
+        "lat": 37.419200000000004,
+        "lng": -122.0574
+    }
+}
+```
+
+The first four properties are always returned if the IP address from the URL was valid. The other properties are returned if found in the GeoLite2 database. Please URL encode the URL in question first.
+
+If you specify the additional URL parameter `lang` by choosing one result language of the following codes ["de", "en", "es", "fr", "ja", "pt-br", "ru", "zh-cn"], you'll receive translated results. For example `http://localhost:8080/?lang=ja&url=http://www.google.com` will return something like
+
+```javascript
+{
+    "url": "www.google.com",
+    "ip": "173.194.39.18",
+    "country": "アメリカ合衆国",
+    "countryIsoCode": "US",
+    "city": "マウンテンビュー",
+    "subdivision": "カリフォルニア州",
+    "location": {
+        "lat": 37.419200000000004,
+        "lng": -122.0574
+    }
 }
 ```
